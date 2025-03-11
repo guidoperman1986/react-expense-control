c20c6c52e773277ca22ff0b829a0d4deaa179aad
Guido Perman <guidoyaelperman@gmail.com>
2025-03-11 15:46:06 -0300

    add background image and enhance LoginTemplate styling; update logo asset

--- src/components/templates/LoginTemplate.tsx
+++ src/components/templates/LoginTemplate.tsx
@@ -1,21 +1,29 @@
 
 import styled from "styled-components";
 import { BtnSave, v } from "../../index";
+import fondo1 from '../../assets/fondo1.svg'; // Import the image
 
 export const LoginTemplate = () => {
   return (
     <Container>
-      <span>Version 1.0</span>
-      <div>
-        <img src="" alt="" />
+      <div className="contentCard">
+        <span className="version">Version 1.0</span>
+        <div className="contentImage">
+          <img src={v.logo} alt="" />
+        </div>
+        <Title>Expenses Control</Title>
+        <p className="frase">Take full control on your earning and expenses</p>
+
+        <ContainerBtn>
+
+          <BtnSave 
+              title="Init with Google" 
+              icon={v.iconogoogle} 
+              bgColor={v.colorSecundario}
+          ></BtnSave>
+        </ContainerBtn>
+
       </div>
-      <Title>Expenses Control</Title>
-      <p>Take full control on your earning and expenses</p>
-
-      <ContainerBtn>
-
-        <BtnSave title="Init with Google" icon={v.iconogoogle} ></BtnSave>
-      </ContainerBtn>
     </Container>
 
 
@@ -24,11 +32,46 @@ export const LoginTemplate = () => {
 }
 /* src/assets/fondo1.svg */
 const Container = styled.div`
-  background-image: url('https://picsum.photos/200/300') no-repeat;
+  background-image: url(${fondo1});
+  background-repeat: no-repeat;
   background-size: cover;
   height: 100vh;
+  color: rgba(255, 255, 255, 0.87);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  .contentCard {
+    background-color: #131313;
+    border-radius: 20px;
+    display: flex;
+    gap: 30px;
+    flex-direction: column;
+    margin: 20px;
+    padding: 20px;
+    box-shadow: 8px 5 18px 3px rgba(0, 0, 0, 0.35);
+
+    .version {
+      color: 727272;
+      text-align: start;
+    }
+
+    .contentImage {
+      img {
+        max-width: 60%;
+      }
+    }
+
+    .frase {
+      color:#909090;
+      font-size: 1.2rem;
+    }
+  }
   
 `;
+
 
 const Title = styled.span`
   font-size: 5rem;
